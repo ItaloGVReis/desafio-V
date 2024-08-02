a221edb03a84754a9c76f44f549e28ac962f41c9
italo Gabriel <italogvr32@gmail.com>
2024-08-02 12:12:12 -0300

    mapa adicionado e botão de aparição

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ function showTab(tabId) {
     var tabs = document.querySelectorAll('.tab-button');
     tabs.forEach(tab => tab.classList.remove('active'));
 
-    document.querySelector(`[onclick="showTab('${tabId}')"]`).classList.add('active');
+    document.querySelector(`[onclick="showTab('${tabId}')]`).classList.add('active');
 
     var panels = document.querySelectorAll('.tab-panel');
     panels.forEach(panel => panel.classList.remove('active'));
