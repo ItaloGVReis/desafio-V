c32c54cc1dcdb28ce4679d2c99af3bb02e5059f9
italo Gabriel <italogvr32@gmail.com>
2024-08-05 21:59:56 -0300

    variaveis ambientes adicionadas

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: 'roundhouse.proxy.rlwy.net',
-    user: 'root',
-    password: 'mDNhTgWoOduytIelPlaqRxgXTnsSniCi',
-    database: 'railway',
-    port: 15794,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect(err => {
--- model/User.js
+++ model/User.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'roundhouse.proxy.rlwy.net',
-    user: 'root',
-    password: 'mDNhTgWoOduytIelPlaqRxgXTnsSniCi',
-    database: 'railway',
-    port: 15794,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 
 });
 
